6d37f819fae851522727240380e58e3373e1410a
ragavanperarasu <ragavanperarasu5761@gmail.com>
2025-06-01 01:05:23 +0530

    modified:   index.js 	new file:   routes/reg2022/syll/sylldownc.js

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const notescomment = require('./routes/reg2022/notes/notescomment')
 
 const syllupl22 = require('./routes/reg2022/syll/syllupl22')
 const syllview = require('./routes/reg2022/syll/syllview')
+const sylldownc = require('./routes/reg2022/syll/sylldownc')
 
 const getpost = require('./routes/reg2022/getpost')
 
@@ -115,6 +116,7 @@ app.use('/',notescomment)
 
 app.use('/',syllupl22)
 app.use('/',syllview)
+app.use('/',sylldownc)
 
 app.use('/',getpost)
 
