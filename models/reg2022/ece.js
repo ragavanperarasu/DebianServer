73266a59b84d69b2091f6cb94dc4bbe5c98bfaeb
ragavanperarasu <ragavanperarasu5761@gmail.com>
2025-06-01 14:37:57 +0530

    new file:   models/reg2022/ece.js

---
+++ models/reg2022/ece.js
@@ -0,0 +1,113 @@
+const mongoose = require("mongoose");
+const reg2025Connection = require("../../database/reg2025");
+
+const commentSchema = new mongoose.Schema({
+  commentby: String,
+  com: String,
+  cdate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const postItemSchema = new mongoose.Schema({
+  docurl: String,
+  postby: String,
+  postbymail: String,
+  postname: String,
+  likes: {
+    type: [String],
+    default: [],
+  },
+  comment: [commentSchema],
+  postdate: {
+    type: Date,
+    default: Date.now,
+  },
+  view: {
+    type: Number,
+    default: 0,
+  },
+  downloadc: {
+    type: Number,
+    default: 0,
+  },
+  likec: {
+    type: Number,
+    default: 0,
+  }
+});
+
+
+const subjectSchema = new mongoose.Schema({
+  subname: String,
+  semqus: [postItemSchema],
+  utoqus: [postItemSchema],
+  uttqus: [postItemSchema],
+  stunote: [postItemSchema],
+  books: [postItemSchema],
+});
+
+const ece = reg2025Connection.model(
+  "ece",
+  new mongoose.Schema({
+    sem: String,
+    sub: [subjectSchema],
+    syllabus: {
+      docurl: String,
+      postby: String,
+      postbymail: String,
+      postname: String,
+      likes: {
+        type: [String],
+        default: [],
+      },
+      comment: [commentSchema],
+      postdate: {
+        type: Date,
+        default: Date.now,
+      },
+      view: {
+        type: Number,
+        default: 0,
+      },
+      downloadc: {
+        type: Number,
+        default: 0,
+      },
+      likec: {
+        type: Number,
+        default: 0,
+      }
+    },
+    timetable: {
+      docurl: String,
+      postby: String,
+      postbymail: String,
+      postname: String,
+      likes: {
+        type: [String],
+        default: [],
+      },
+      comment: [commentSchema],
+      postdate: {
+        type: Date,
+        default: Date.now,
+      },
+      view: {
+        type: Number,
+        default: 0,
+      },
+      downloadc: {
+        type: Number,
+        default: 0,
+      },
+      likec: {
+        type: Number,
+        default: 0,
+      }
+    },
+  })
+);
+
+module.exports = ece;
